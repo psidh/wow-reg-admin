1e7c269cfd59b9f6bd996f42aff2583a4e1c26d3
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-06-07 14:54:35 +0530

    styling

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -2,7 +2,7 @@ export default function Detail({ email }: { email: string }) {
   if (email) {
     return (
       <div>
-        <div className=" bg-neutral-800 rounded-xl shadow-2xl shadow-neutral-600">
+        <div className="p-2 bg-neutral-800 rounded-xl shadow-2xl shadow-neutral-600">
           <h2>Email: {email}</h2>
         </div>
       </div>
