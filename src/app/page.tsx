e022bb6d680d290ec4cadbb5828313d335df9920
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-06-07 14:48:42 +0530

    added Details.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
-import { Scanner, IDetectedBarcode } from "@yudiel/react-qr-scanner";
+import { Scanner } from "@yudiel/react-qr-scanner";
 import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import Detail from "@/components/Detail";
 
 export default function Home() {
   const [data, setData] = useState<any | null>([
@@ -10,16 +12,19 @@ export default function Home() {
   ]);
 
   const email = data[0].rawValue || "";
+  if (email !== "") {
+    toast.success("Found Result");
+  }
   console.log(email);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen py-12">
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Toaster />
+      <h1 className="py-8">Admin | Scanner</h1>
       <Scanner onScan={(result) => setData(result)} />
       {data ? (
         <>
-          <div>
-            <h2>Email: {email}</h2>
-          </div>
+          <Detail email={email} />
         </>
       ) : (
         <>
---
+++ src/components/Detail.tsx
@@ -0,0 +1,13 @@
+export default function Detail({ email }: { email: string }) {
+  if (email) {
+    return (
+      <div>
+        <div className=" bg-neutral-800 rounded-xl shadow-2xl shadow-neutral-600">
+          <h2>Email: {email}</h2>
+        </div>
+      </div>
+    );
+  } else {
+    <div>Scan</div>;
+  }
+}
