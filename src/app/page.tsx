511f1c6476c5afb3c7c068d34ccd439a22d688e9
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-06-07 14:36:23 +0530

    added scanner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
-"use client"
-import { Scanner } from "@yudiel/react-qr-scanner";
-import Image from "next/image";
+"use client";
+import { Scanner, IDetectedBarcode } from "@yudiel/react-qr-scanner";
+import { useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState<any | null>([
+    {
+      rawValue: "",
+    },
+  ]);
+
+  const email = data[0].rawValue || "";
+  console.log(email);
+
   return (
-    <div>
-      hi there
-      <Scanner onScan={(result) => console.log(result)} />
+    <div className="flex flex-col items-center justify-center h-screen py-12">
+      <Scanner onScan={(result) => setData(result)} />
+      {data ? (
+        <>
+          <div>
+            <h2>Email: {email}</h2>
+          </div>
+        </>
+      ) : (
+        <>
+          <p>Scan QR</p>
+        </>
+      )}
     </div>
   );
 }
